caffeb88541cfd9cff87ad5c6465f78b8f56c867
Farena <fedarena@gmail.com>
2020-07-24 16:06:14 +0100

    Factories

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Product;
+use Faker\Generator as Faker;
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence(),
+        'description' => $faker->paragraph(),
+        'is_approved' => $faker->boolean,
+        'price' => $faker->randomFloat(2,0,2000),
+    ];
+});
